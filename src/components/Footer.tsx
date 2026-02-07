20f9ee9fe2d3a9f85301be93674bf223ed332646
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-07 11:45:25 -0700

    feat: add note

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,21 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { Link, Stack, Text } from "@chakra-ui/react";
 import { ChangelogButton } from "./ChangelogButton";
 
 export default function Footer() {
   return (
     <Stack align="center">
-      <Text fontSize="xs" color="fg.muted" textAlign="center" mt={8}>
+      <Text fontSize="xs" color="fg.muted" textAlign="center">
         Stealing Luke's job since 2026
+      </Text>
+      <Text fontSize="xs" color="fg.muted" textAlign="center">
+        Found a bug, or want to request a feature? Open a{" "}
+        <Link
+          href="https://github.com/gadomski/quick-five/issues"
+          target="_blank"
+        >
+          Github issue
+        </Link>
+        .
       </Text>
       <ChangelogButton fontSize="xs" color="fg.muted" />
     </Stack>
