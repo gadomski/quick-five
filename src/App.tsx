af61844b6b3ddffdf5a15b622a0585d7cea6d16d
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-07 07:15:25 -0700

    fix: container width

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Container, VStack, Text } from "@chakra-ui/react";
-import { GameHeader } from "./components/GameHeader";
+import Header from "./components/Header";
 import { PlayerList } from "./components/PlayerList";
 import { AddPlayerButton } from "./components/AddPlayerButton";
 
 function App() {
   return (
-    <Container maxW="container.md" py={4} pt={4}>
+    <Container maxW="md" py={4}>
       <VStack gap={4} align="stretch">
-        <GameHeader />
+        <Header />
         <PlayerList />
         <AddPlayerButton />
-        <Text fontSize="xs" color="fg.muted" textAlign="center" mt={8}>
+        <Text fontSize="xs" color="fg.muted" textAlign="center">
           Stealing Luke's job since 2026
         </Text>
       </VStack>
